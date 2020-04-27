27ad58f8e5d640712422b5c2f9ffc4d536188f5c
SergeyEzhin <ezhin@mail.ru>
2020-04-27 18:03:58 +0300

    Start Avalon

---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+console.log('hi');
+import '../scss/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,125 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDev = process.env.NODE_ENV = 'development';
+const pages = ['index'];
+
+const webpackConfig = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill','./js/main.js'],
+    output: {
+        filename: "./js/bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: [path.resolve(__dirname, "dist"), path.resolve(__dirname, "src")],
+        historyApiFallback: true,
+        noInfo:true,
+        overlay: true,
+        port: 4200,
+        hot: true
+    },
+    devtool: isDev ? 'source-map': '',
+    plugins: [
+        new CopyWebpackPlugin(
+            [
+                {
+                    from: './img',
+                    to: './img'
+                }
+            ]
+        ),
+        new MiniCssExtractPlugin({
+            filename: './css/style.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test:  /\.(png|jpg|svg|csv|xml|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader?name=./img/[name].[ext]'
+                    }
+                ]
+            },
+            {
+                test:  /\.(ttf|woff|woff2|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+
+                            name: '[name].[ext]',
+                            outputPath: '/fonts',
+                            publicPath: '../fonts'
+                        }
+
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: [
+                    // {
+                    //     loader: 'eslint-loader'
+                    // },
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env'
+                            ]
+                        }
+                    }
+                ],
+                exclude: '/node_modules/'
+            }
+
+        ]
+    }
+}
+
+allPages = () =>
+{
+    pages.forEach((file) => {
+        webpackConfig.plugins.push(
+            new HTMLWebpackPlugin({
+                filename: `${file}.html`,
+                template: `./${file}.html`
+            })
+        );
+    });
+}
+
+allPages();
+
+module.exports = webpackConfig;
+
