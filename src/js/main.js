b1a85e4e22814a910cd6567671de25892fc3fbb4
SergeyEzhin <ezhin@mail.ru>
2020-04-28 18:56:35 +0300

    Half main page is done

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,45 @@
 console.log('hi');
 import '../scss/style.scss';
+
+
+var imgSvg = document.querySelectorAll('.img-svg');
+
+for(var i = 0; i < imgSvg.length; i++)
+{
+    var img = imgSvg[i];
+    var imgClass = img.getAttribute('class');
+    var imgUrl = img.getAttribute('src');
+
+    var request = new XMLHttpRequest();
+    request.open('GET', imgUrl, false);
+
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          var data = this.response;
+          var parser = new DOMParser();
+          var htmlSvg = parser.parseFromString(data, 'text/html');
+          var svg = htmlSvg.querySelector('svg');
+
+          svg.setAttribute('class', 'img-svg');
+          console.log(svg);
+
+          // img.replaceWith(svg);
+          img.parentElement.insertBefore(svg, img);
+          img.parentElement.removeChild(img);
+          
+        }
+        else 
+        {
+          console.log('error!');
+        }
+      };
+      
+      request.onerror = function(e) {
+          console.log(e);
+        // There was a connection error of some sort
+      };
+      
+      request.send();
+}
+
